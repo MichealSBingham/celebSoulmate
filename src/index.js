42d7f72b10c684f8ed77d0b788f03c8066ddc70c
Micheal Bingham <micheal@findamare.com>
2022-07-12 15:34:38 -0400

    web app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Question extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.question}</h1>
+      </div>
+    );
+  }
+}
+
+
+
+  const linkToImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Venus_symbol.svg/2048px-Venus_symbol.svg.png";
+
+  class AmareLogo extends React.Component{
+
+    
+    render(){ 
+      return(
+        <div className="amare-logo">
+          <img src={linkToImage} alt="Amare Logo" />
+        </div>
+      );
+    }
+  }
+
+
+
+
+  class App extends React.Component{
+    render(){
+      return(
+        <div className="app">
+          <AmareLogo />
+          <Question question="Let's Ask The Stars To Find Your Celebrity..." />
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<App />);
+  
