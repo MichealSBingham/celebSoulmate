b7a23e9d40f957ce79006b7baa1016c09fbad048
Micheal Bingham <micheal@findamare.com>
2022-07-13 17:05:25 -0400

    Added actions on soulmate and twin buttons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,52 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
+//import logo from './images/logo.png'; // 
 
+
+
+// Images 
+
+//- TODO:  This should be local
+const logo2 = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Venus_symbol.svg/2048px-Venus_symbol.svg.png";
+const logo = "https://i.ibb.co/vch2ZC4/amare-logo-no-background.png";
+const backgroundStyle = { backgroundColor: 'pink', 
+backgroundSize: 'cover',
+
+backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh', 
+        
+};
+
+//=========================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First "Page" of the app ==========================================================
+// The Text Element that displays the text/question for the user 
 
 class Question extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
@@ -14,8 +57,33 @@ class Question extends React.Component {
 }
 
 
+// Button for displaying the answer to the question
+class Button extends React.Component{
 
-  const linkToImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Venus_symbol.svg/2048px-Venus_symbol.svg.png";
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // Changes state of parent 
+  handleClick = (choice) => {
+
+    console.log("Clicked button: " + choice);
+    // Sets parent state depending on the button clicked
+    this.props.didChoose(choice);
+  }
+
+
+
+  render(){
+    return(
+      <div>
+        <button onClick={() => this.handleClick(this.props.choice)}>{this.props.buttonText}</button>
+      </div>
+    );
+  }
+}
+
 
   class AmareLogo extends React.Component{
 
@@ -23,7 +91,8 @@ class Question extends React.Component {
     render(){ 
       return(
         <div className="amare-logo">
-          <img src={linkToImage} alt="Amare Logo" />
+          <img src={logo} alt="Amāre Logo" />
+         
         </div>
       );
     }
@@ -31,13 +100,65 @@ class Question extends React.Component {
 
 
 
+  class InitialPage extends React.Component{
 
-  class App extends React.Component{
+    constructor(props) {
+      super(props);
+      this.didChoose.bind(this);
+      this.state = {
+        question: "Let's Ask The Stars To Find Your Celebrity... ",
+       };
+
+      
+      }
+
+       // This function is called when the user clicks on the button
+      didChoose = (choice) => {
+        console.log('You clicked the button: ' + choice);
+        this.setState({
+          question: choice == "twin" ? "Let's find your twin!" : "Let's Find Your Soulmate!",
+        });
+      }
+
+      
+
     render(){
       return(
-        <div className="app">
+        <div >
+          
+          <Question question = {this.state.question} />
+          
+          <Button buttonText="Soulmate 😍" choice="soulmate" didChoose={this.didChoose}  />
+          <Button buttonText="Twin 👯" choice="twin" didChoose={this.didChoose}  />
+        </div>
+      );
+    }   
+
+
+
+  }
+
+// End of First "Page" of the app ==========================================================
+
+
+
+
+
+
+
+
+
+
+  class App extends React.Component{
+
+    
+
+
+    render(){
+      return(
+        <div className="app" style={backgroundStyle}>
           <AmareLogo />
-          <Question question="Let's Ask The Stars To Find Your Celebrity..." />
+          <InitialPage />
         </div>
       );
     }
