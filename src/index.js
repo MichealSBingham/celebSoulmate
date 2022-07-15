c3543b81bd57769400f41d6581d3a8d534251985
Micheal S. Bingham <micheal@xiris.ai>
2022-07-15 16:10:46 -0400

    adding more stuff to web app

--- src/index.js
+++ src/index.js
@@ -85,7 +85,7 @@ class Button extends React.Component{
 }
 
 
-  class AmareLogo extends React.Component{
+class AmareLogo extends React.Component{
 
     
     render(){ 
@@ -99,24 +99,38 @@ class Button extends React.Component{
   }
 
 
-
-  class InitialPage extends React.Component{
+class InitialPage extends React.Component{
 
     constructor(props) {
       super(props);
       this.didChoose.bind(this);
+      this.goToPage.bind(this); 
+
       this.state = {
         question: "Let's Ask The Stars To Find Your Celebrity... ",
+        page: "Home"
        };
 
       
       }
 
        // This function is called when the user clicks on the button
+       
       didChoose = (choice) => {
         console.log('You clicked the button: ' + choice);
         this.setState({
           question: choice == "twin" ? "Let's find your twin!" : "Let's Find Your Soulmate!",
+        });
+      }
+      
+
+
+      // Changes pages for the registration 
+      goToPage = (page) => {
+
+        console.log('Go to page: ' + page );
+        this.setState({
+          page: {page}
         });
       }
 
@@ -126,10 +140,17 @@ class Button extends React.Component{
       return(
         <div >
           
-          <Question question = {this.state.question} />
+          <AmareLogo />
+
+          <Question question={this.state.question} />
           
-          <Button buttonText="Soulmate 😍" choice="soulmate" didChoose={this.didChoose}  />
-          <Button buttonText="Twin 👯" choice="twin" didChoose={this.didChoose}  />
+          <RegistrationSection page={this.state.page} pageSetter={this.goToPage} /> 
+
+
+          <PageControl /> 
+
+
+        
         </div>
       );
     }   
@@ -138,6 +159,103 @@ class Button extends React.Component{
 
   }
 
+  
+
+  // Registration Info Section ---> 
+
+class RegistrationSection extends React.Component{
+
+
+    // page: 
+      // Home, Name, Gender, Orientation, Birthlocation, Birthday
+    constructor(props){
+      super(props); 
+
+
+    }
+
+
+    // Whether or not user picked soulmate or twin 
+    didPick = (choice) => {
+
+      console.log('Did pick: ' + choice)
+
+      // Go to a page 
+      this.props.pageSetter('Home')
+
+    }
+
+    render(){
+
+      const page = this.props.page 
+
+      if (page == 'Home'){
+
+        return ( 
+        
+           <div>
+          
+          <Button buttonText="Soulmate 😍" choice="soulmate" didChoose={this.didPick}  />
+          <Button buttonText="Twin 👯" choice="twin" didChoose={this.didPick}  />
+           </div>
+           
+           );
+       
+      }
+
+      else if (page == 'Name'){
+
+        return(
+
+          <div>
+            <h1> This is where we ask for the user's name </h1>
+          </div>
+        );
+      }
+
+      else if (page == 'Gender'){
+
+      }
+
+      else if (page == 'Orientation'){
+
+      }
+
+      else if (page == 'Birthlocation'){
+
+      }
+
+      else if (page == 'Birthday'){
+
+      }
+
+      else {
+        return null;
+      }
+
+    }
+  }
+
+
+
+  // Page Control 
+
+  class PageControl extends React.Component{
+    
+    constructor(props){
+      super(props); 
+    }
+
+    render(){
+
+      return (
+                <h1> Page Control </h1>
+      );
+      
+    }
+  }
+
+  
 // End of First "Page" of the app ==========================================================
 
 
@@ -155,9 +273,11 @@ class Button extends React.Component{
 
 
     render(){
+
+     // const isRegistering = this.state.isRegisering;
+
       return(
         <div className="app" style={backgroundStyle}>
-          <AmareLogo />
           <InitialPage />
         </div>
       );
